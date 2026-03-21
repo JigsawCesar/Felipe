ee918acf7a9f85cd4536ed6b7d20ecbffe1c1867
Felipe <felipeportela_@hotmail.com>
2026-03-21 13:00:59 -0300

    Organizando exercícios e finalizando do-while

--- Turma_manhã/Exercicio_Switch/atividade_1.js
+++ Turma_manhã/Exercicio_Switch/atividade_1.js
@@ -1,36 +1,42 @@
 const prompt = require(`prompt-sync`)();
 
-console.log(`----- Menu de Seleção ------\n| [1] - 👊 Ação            |\n| [2] - 🤣 Comédia         |
-| [3] - 👻 Terror          |\n| [4] - 🤖 Animação        |\n---------------------------`);
-let genero = parseInt(prompt(`Digite o número do menu correspondente: `));
+let menu;
+do {
+console.log(`----- Menu de Seleção -----\n| [1] - 👊 Ação            |\n| [2] - 🤣 Comédia         |
+| [3] - 👻 Terror          |\n| [4] - 🤖 Animação        |\n|                         |
+| [0] - ❌ Sair           |\n---------------------------`);
+menu = parseInt(prompt(`Digite o número correspondente ao gênero desejado: `));
 
-switch (genero) {
-    case 1:
+    switch (menu) {
+        case 1:
         
-        console.log(`O filme passará na Sala 1`);
+            console.log(`- O filme passará na Sala 1`);
         break;
-    case 2:
-        
-        console.log(`O filme passará na Sala 2`);
-        break;
-    
-    case 3:
-        
-        console.log(`O filme passará na Sala 3`);
-        break;
-    
-    case 4:
 
-        console.log(`O filme passará na Sala 4`);
+        case 2:
+        
+            console.log(`- O filme passará na Sala 2`);
         break;
     
-    default:
-        do {
+        case 3:
+        
+            console.log(`- O filme passará na Sala 3`);
+        break;
+    
+        case 4:
+
+            console.log(`- O filme passará na Sala 4`);
+        break;
+
+        case 0:
             
-        } while (condition);
-        console.log(`----- Menu de Seleção ------\n| [1] - 👊 Ação            |\n| [2] - 🤣 Comédia         |
-        | [3] - 👻 Terror          |\n| [4] - 🤖 Animação        |\n---------------------------`);
-        genero = parseInt(prompt(`Digite o número do menu correspondente: `));
+            console.log(`Encerrando o menu...`);
+        break;
+
+        default:
+            console.log(`\n😅 Opção inválida, tente novamente!`);
         
         break;
-}
+    }
+
+} while (menu != 0 && menu != 1 && menu != 2 && menu != 3 && menu != 4);
